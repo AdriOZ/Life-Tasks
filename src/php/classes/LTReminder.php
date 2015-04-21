131d27933975abbdac9ced8fbe0cd061b1e79a38
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-04-21 15:41:05 +0200

    Añadido constructor y función de ejecución
    Creado el constructor y el punto de acceso único de ejecución de la
    clase.

--- src/php/classes/LTReminder.php
+++ src/php/classes/LTReminder.php
@@ -1,4 +1,10 @@
 <?php
 class LTReminder extends LTResponse {
-	
+	public function __construct( $action, $where ) {
+		parent::__construct( $action, $where );
+	}
+
+	public function execute () {
+		# TODO
+	}
 }

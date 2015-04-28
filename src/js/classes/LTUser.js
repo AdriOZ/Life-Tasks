1202c19793e052da583a9834ae14ab1ea9b1b1b6
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-04-28 11:48:44 +0200

    Creado constructor de clase User.

---
+++ src/js/classes/LTUser.js
@@ -0,0 +1,16 @@
+(function ( global ) {
+global.LT = global.LT || {};	// Namespace
+
+/**
+ * User class.
+ * @param {number} id       Identifier.
+ * @param {string} email    Email of the account.
+ * @param {string} password Passwod of the account.
+ */
+LT.User = function ( id, email, password ) {
+	this._id = id;						// Identifier of the user
+	this._email = email;				// Email of the account
+	this._password = md5( password );	// Password of the user
+	this._notebooks = [];				// Array of notebooks
+};
+}) ( window );

61df0ca0ef1f3f10c476ec8e960c488b5889c1c6
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-04-28 13:29:29 +0200

    Creada función para obtener el tipo de almacenamiento.

--- src/js/classes/LTStorage.js
+++ src/js/classes/LTStorage.js
@@ -11,4 +11,25 @@ var NO_STORAGE = 0,			// Cookies disabled and no localStorage.
 
 var typeOfStorage = getTypeOfStorage(); // The used type storage.
 
+/**
+ * Get the type of used storage.
+ * @return {number} NO_STORAGE, COOKIE_STORAGE or WEB_STORAGE.
+ */
+function getTypeOfStorage () {
+	var typeOfStorage;
+
+	try {
+		localStorage.setItem( 'test', 'test' );
+		localStorage.removeItem( 'test' );
+		typeOfStorage = WEB_STORAGE;
+	} catch ( e ) {
+		if ( navigator.cookieEnabled ) {
+			typeOfStorage = COOKIE_STORAGE;
+		} else {
+			typeOfStorage = NO_STORAGE;
+		}
+	}
+
+	return typeOfStorage;
+}
 })( window, $ );

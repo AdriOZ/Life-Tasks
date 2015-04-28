6f9a290c11e194acde4ff698e41799b38c72aab3
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-04-28 13:25:07 +0200

    Creadas constantes de almacenamiento y variable que indica el tipo de almacenamiento.

--- src/js/classes/LTStorage.js
+++ src/js/classes/LTStorage.js
@@ -3,4 +3,12 @@ global.LT = global.LT || {};	// Namespace
 
 /* Object */
 LT.Storage = new LT.User( -1, '', '' );		// Default user account
+
+/* util */
+var NO_STORAGE = 0,			// Cookies disabled and no localStorage.
+	COOKIE_STORAGE = 1,		// Must use cookies because localStorage isn't supported.
+	WEB_STORAGE = 2;		// localStorage supported.
+
+var typeOfStorage = getTypeOfStorage(); // The used type storage.
+
 })( window, $ );

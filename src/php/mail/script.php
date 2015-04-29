7d4bd21f223896647fb96b2bf0cebfadabe6fa1b
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-04-29 10:27:42 +0200

    Creada función que obtiene los recordatorios.

--- src/php/mail/script.php
+++ src/php/mail/script.php
@@ -2,5 +2,16 @@
 require 'PHPMailer-master/class.phpmailer.php';		# To send emails
 require '../classes/bbdd.php';						# Connect to the database
 
-# Functions
-# Begin of the script
+############################# Functions #############################
+/**
+ * Search the reminders that must be sended.
+ * @return [array] Array of reminders.
+ */
+function getReminders () {
+	return Database::query(
+		"SELECT id_reminder,note FROM reminders WHERE sent=0 AND d_reminder
+		<= sysdate()"
+	);
+}
+
+######################### Begin of the script ########################

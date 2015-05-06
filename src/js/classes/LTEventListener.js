8506492c04159c652f358998eba0c501f14b0d23
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-06 09:01:33 +0200

    Añadido al index el fichero y creadas funciones manejadoras del login y el registro

--- src/js/classes/LTEventListener.js
+++ src/js/classes/LTEventListener.js
@@ -6,6 +6,20 @@ global.LT = global.LT || {};	// Namespace
  * @type {Object}
  */
 LT.EventListener = {
+	/**
+	 * Makes the sign in request with data from the form.
+	 * @param  {object} element HTML form.
+	 */
+	signIn: function ( element ) {
+		/* TODO */
+	},
 
+	/**
+	 * Makes the sign up request with the data from the form.
+	 * @param  {object} element HTML form.
+	 */
+	signUp: function ( element ) {
+		/* TODO */
+	}
 };
 })( window, $ );

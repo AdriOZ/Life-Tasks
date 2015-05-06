69b438910dfbcf0db853d3b55d54b41ca2288a20
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-06 08:50:41 +0200

    Creado controlador de Eventos del DOM

---
+++ src/js/classes/LTEventListener.js
@@ -0,0 +1,11 @@
+(function ( global, $ ) {
+global.LT = global.LT || {};	// Namespace
+
+/**
+ * Contains the functions that controls the events in the DOM.
+ * @type {Object}
+ */
+LT.EventListener = {
+
+};
+})( window, $ );

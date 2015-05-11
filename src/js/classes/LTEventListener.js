9c39a869c0a115c6b637735b4ad0abcd96788e61
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-11 09:50:52 +0200

    Primera aproximación de función para hacer login

--- src/js/classes/LTEventListener.js
+++ src/js/classes/LTEventListener.js
@@ -11,7 +11,23 @@ LT.EventListener = {
 	 * @param  {object} element HTML form.
 	 */
 	signIn: function ( element ) {
-		/* TODO */
+		var inputs = $( element ).find( 'input' );
+
+		/* Temporal user to keep the data safe */
+		var tmpUser = new LT.User( -1, '', '' );
+		tmpUser._email = inputs.first().val();
+		tmpUser.setPassword( inputs.last().val() );
+
+		/* Replacing password to md5 cyphered password */
+		$( element ).find( 'input' ).last().val( tmpUser._password );
+
+		/* Making the request */
+		LT.RequestMaker.makeLogin(
+			element,
+			function ( data ) {
+				// TODO
+			}
+		);
 	},
 
 	/**

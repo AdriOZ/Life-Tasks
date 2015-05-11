82796d2d29726d135d68b2daaea48ede8e0ed511
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-11 09:03:39 +0200

    Avance en script de carga de html

--- src/js/classes/LTHTML.js
+++ src/js/classes/LTHTML.js
@@ -1,4 +1,38 @@
 (function ( global, $ ) {
 global.LT = global.LT || {};	// Namespace
 
+/**
+ * Contains functions that load the html into the page.
+ * @type {Object}
+ */
+LT.HTML = (function () {
+	// Private properties
+	var _device,		// Device: desktop or mobile
+		_generalPath,	// Path to pages that are common in both kind of devices
+		_specificPath,	// Path to pages specific for desktop or mobiles
+		_sections,		// Array of sections
+		_modals;		// Array of modals dialogues
+
+	// Initializing properties
+	_device = _isMobile() ? 'mobile' : 'desktop';
+	_generalPath = 'html/';
+	_specificPath = _generalPath + _device + '/';
+	_sections = {
+		// TODO
+	};
+	_modals = {
+		// TODO
+	};
+	
+	// Returns true if the device is a mobile.
+	function _isMobile () {
+		return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+				.test( navigator.userAgent );
+	}
+
+	// Closures
+	return {
+
+	};
+});
 })( window, $ );

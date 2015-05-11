27277ad0b7dfb8f6b158a5f79588ca4c1c62eacd
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-11 16:01:55 +0200

    Revert "Revert ed33fdc..2df7eee"
    This reverts commit 2f5534a794056d57df871b5916343e32d6327464.

--- src/js/classes/LTEventListener.js
+++ src/js/classes/LTEventListener.js
@@ -8,10 +8,38 @@ global.LT = global.LT || {};	// Namespace
 LT.EventListener = {
 	/**
 	 * Makes the sign in request with data from the form.
-	 * @param  {object} element HTML form.
 	 */
-	signIn: function ( element ) {
-		/* TODO */
+	signIn: function () {
+		var element = $( 'form' )[ 0 ];
+		var inputs = $( element ).find( 'input' );
+
+		/* Temporal user to keep the data safe */
+		var tmpUser = new LT.User( -1, '', '' );
+		tmpUser._email = inputs.first().val();
+		tmpUser.setPassword( inputs.last().val() );
+
+		/* Replacing password to md5 cyphered password */
+		$( element ).find( 'input' ).last().val( tmpUser._password );
+
+		/* Making the request */
+		LT.RequestMaker.makeLogin(
+			new FormData( element ),
+			function ( data ) {
+				if ( data.status == LT.Communicator.SUCCESS ) {
+					// Loading credentials
+					LT.Storage._email = tmpUser._email;
+					LT.Storage._password = tmpUser._password;
+					LT.Storage._id = data.uid;
+					LT.Storage._notebooks = [];
+
+					// Loading view
+					LT.HTML.loadLogin();
+				} else {
+					// TODO
+				}
+			}
+		);
+		return false;
 	},
 
 	/**

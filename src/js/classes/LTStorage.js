972a4e28e0e050a4c9fb51e86ddbcad75acb41e3
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-12 11:50:27 +0200

    Recuperados métodos de exportación e importación de datos

--- src/js/classes/LTStorage.js
+++ src/js/classes/LTStorage.js
@@ -4,4 +4,73 @@ global.LT = global.LT || {};	// Namespace
 /* Object */
 LT.Storage = new LT.User( -1, '', '' );		// Default user account
 
+var NO_STORAGE = 0,			// Cookies disabled and no localStorage.
+	COOKIE_STORAGE = 1,		// Must use cookies because localStorage isn't supported.
+	WEB_STORAGE = 2;		// localStorage supported.
+
+var typeOfStorage = getTypeOfStorage(); // The used type storage.
+
+/**
+ * Get the type of used storage.
+ * @return {number} NO_STORAGE, COOKIE_STORAGE or WEB_STORAGE.
+ */
+function getTypeOfStorage () {
+	var typeOfStorage;
+
+	try {
+		localStorage.setItem( 'test', 'test' );
+		localStorage.removeItem( 'test' );
+		typeOfStorage = WEB_STORAGE;
+	} catch ( e ) {
+		if ( navigator.cookieEnabled ) {
+			typeOfStorage = COOKIE_STORAGE;
+		} else {
+			typeOfStorage = NO_STORAGE;
+		}
+	}
+
+	return typeOfStorage;
+}
+
+/**
+ * Import data from the storage
+ */
+function importData () {
+	var aux;	// Parsed object
+
+	if ( typeOfStorage === WEB_STORAGE ) {
+		// Checks if there is data
+		if ( localStorage.getItem( 'storage' ) ) {
+			aux = JSON.parse( localStorage.getItem( 'storage' ) );
+
+			// Importing data
+			LT.Storage._id = aux.id;
+			LT.Storage._email = aux.email;
+			LT.Storage._password = aux.password;
+		}
+	} else if ( typeOfStorage === COOKIE_STORAGE ) {
+		// With cookies, only store the id, email and password.
+		if ( $.cookie( 'id' )
+			&& $.cookie( 'email' )
+			&& $.cookie( 'password' ) ) {
+			LT.Storage._id = $.cookie( 'id' );
+			LT.Storage._email = $.cookie( 'email' );
+			LT.Storage._password = $.cookie( 'password' );
+		}
+	}
+}
+
+/**
+ * Exports all data into the storage used.
+ */
+function exportData () {
+	var COOKIES_LIFE = 30;	// Days to live.
+	if ( typeOfStorage === WEB_STORAGE ) {
+		localStorage.setItem( 'storage', JSON.stringify( LT.Storage ) );
+	} else if ( typeOfStorage === COOKIE_STORAGE ) {
+		$.cookie( 'id', LT.Storage._id, { expires: COOKIES_LIFE } );
+		$.cookie( 'email', LT.Storage._email, { expires: COOKIES_LIFE } );
+		$.cookie( 'password', LT.Storage._password, { expires: COOKIES_LIFE } );
+	}
+}
 })( window, $ );

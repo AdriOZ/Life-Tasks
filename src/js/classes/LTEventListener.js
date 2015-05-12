bb331ec234a4f792029f637885d906b3d1c75af4
Adrian Ocaña Zamora <adrianozamora@gmail.com>
2015-05-12 15:33:07 +0200

    Creada función de registro de usuarios

--- src/js/classes/LTEventListener.js
+++ src/js/classes/LTEventListener.js
@@ -33,7 +33,7 @@ LT.EventListener = {
 						LT.Storage._id = data.uid;
 						LT.Storage._notebooks = [];
 						LT.Storage.loadEverything();
-						
+
 						// Loading view
 						LT.HTML.loadLogin();
 					} else {
@@ -53,8 +53,44 @@ LT.EventListener = {
 	 * Makes the sign up request with the data from the form.
 	 * @param  {object} element HTML form.
 	 */
-	signUp: function ( element ) {
-		/* TODO */
+	signUp: function () {
+		var element = $( 'form' )[ 1 ];
+		var inputs = $( element ).find( 'input' );
+
+		if ( inputs[ 0 ].value && inputs[ 1 ].value ) {
+			/* Temporal user to keep the data safe */
+			var tmpUser = new LT.User( -1, '', '' );
+			tmpUser._email = inputs.first().val();
+			tmpUser.setPassword( inputs.last().val() );
+
+			/* Replacing password to md5 cyphered password */
+			$( element ).find( 'input' ).last().val( tmpUser._password );
+
+			/* Making the request */
+			LT.RequestMaker.insert.user(
+				new FormData( element ),
+				function ( data ) {
+					if ( data.status == LT.Communicator.SUCCESS ) {
+						// Loading credentials
+						LT.Storage._email = tmpUser._email;
+						LT.Storage._password = tmpUser._password;
+						LT.Storage._id = data.uid;
+						LT.Storage._notebooks = [];
+						LT.Storage.loadEverything();
+						
+						// Loading view
+						LT.HTML.loadLogin();
+					} else {
+						LT.HTML.simpleModalDialogue(
+							$( '#sign_up_in' ),
+							'Email is already taken'
+						);
+						$( element ).find( 'input' ).last().val( '' );
+					}
+				}
+			);
+		}
+		return false;
 	}
 };
 })( window, $ );
